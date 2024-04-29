9955bc85605783ed7b185a3ecb988b2b928ec987
Phucego <phucdinhthanh2312@gmail.com>
2024-04-29 13:35:29 +0700

    29/4 level select build

--- Assets/Scripts/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelectManager.cs
@@ -5,10 +5,37 @@ using UnityEngine.SceneManagement;
 public class LevelSelectManager : MonoBehaviour
 {
 
-    public int mapID;
+
     private Scene scene;
-    
-    
-    
-    
+    [SerializeField] public List<GameObject> portals = new List<GameObject>();
+
+    LEVELS levels;
+    public enum LEVELS
+    {
+        //assign the levels to an integer
+        LEVELTUT = 1000, LEVEL1 = 1001, LEVEL2 = 1002, LEVEL3 = 1003
+    }
+    void LevelSelection()
+    {
+        switch (levels)
+        {
+            case LEVELS.LEVELTUT:
+                SceneManager.LoadScene(1000);
+                break;
+            case LEVELS.LEVEL1:
+                SceneManager.LoadScene(1001);
+                break;
+            case LEVELS.LEVEL2:
+                SceneManager.LoadScene(1002);
+                break;
+            case LEVELS.LEVEL3:
+                SceneManager.LoadScene(1003);
+                break;                
+        }
+
+    }
+
+
+
+
 }
